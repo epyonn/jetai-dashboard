1105aa05c973c87320a6fb24104aa7542c189c8f
Epyonn <pdavisnguyen1@outlook.com>
2024-03-15 12:20:35 -0700

    feat(style): update styling for responsivenss

--- components/ComparisonResults.tsx
+++ components/ComparisonResults.tsx
@@ -1,8 +1,8 @@
-/** 
- * File Name: ComparisonResults.tsx
- * Purpose: Renders a table displaying the comparison results of jets, including their rank, name, and value, based on a selected comparison criteria.
- * Created Date: 2024-03-14 
- */
+/*File Name: ComparisonResults.tsx
+* Purpose: Renders a table displaying the comparison results of jets, including their rank, name, and value, based on a selected comparison criteria.
+* Created Date: 2024-03-14 
+*/
+
 import React from 'react';
 import { ComparisonResult } from '@/types/types'; 
 
@@ -11,7 +11,7 @@ interface ComparisonResultsProps {
 }
 
 const ComparisonResults: React.FC<ComparisonResultsProps> = ({ results }) => (
-  <div className="flex flex-col sm:w-1/4 md:w-1/3">
+  <div className="flex flex-col w-full md:w-1/2 lg:w-1/4 mx-auto">
     <label className="block text-sm font-medium text-gray-700">Comparison Results:</label>
     <div className="overflow-x-auto rounded-md">
       <table className="min-w-full table-auto divide-y divide-gray-200 shadow-lg border border-gray-200 rounded-md">
